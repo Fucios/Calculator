1fc8f70b6167f8ccf1ef120478404bd8953163d1
Fucios <196479439+Fucios@users.noreply.github.com>
2025-02-05 17:43:12 -0300

    Calculator Initial Model - Completed

--- calcScript.js
+++ calcScript.js
@@ -5,3 +5,23 @@ function substract() {}
 function multiply() {}
 
 function divide() {}
+
+let numbersContainer = document.getElementById("numbers")
+
+function buttonsModel(btn) {
+        btn.style.width = "25%"
+        btn.style.height = "auto"}
+
+const buttons = [["(",",",")","÷"],["1","2","3", "*"],["4","5","6", "-"], ["7","8","9", "+"],["0",".","C", "="]]
+
+for (let i = 0; i < buttons.length; i++) {
+    let charsIndex = buttons[i] 
+    
+    for (let j = 0; j < charsIndex.length; j++) {
+        let calcButtons = document.createElement("button")
+        calcButtons.textContent = charsIndex[j]
+        calcButtons.style.fontSize = "25px"
+        buttonsModel(calcButtons)
+        
+        numbersContainer.appendChild(calcButtons)
+}}
